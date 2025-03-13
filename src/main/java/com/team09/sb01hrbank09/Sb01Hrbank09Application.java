2fb62baccad04213dde8fbb20cabc1a0ed32814d
GiMin0123 <minby260306@gmail.com>
2025-03-13 15:42:00 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/team09/sb01hrbank09/Sb01Hrbank09Application.java
@@ -0,0 +1,13 @@
+package com.team09.sb01hrbank09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb01Hrbank09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb01Hrbank09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team09/sb01hrbank09/Sb01Hrbank09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team09.sb01hrbank09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb01Hrbank09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
