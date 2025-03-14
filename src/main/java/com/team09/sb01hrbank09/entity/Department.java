1053947d4adcac94eb6c43c1897e0d41603e7167
김승찬 <tmdcks801>
2025-03-14 14:37:36 +0900

    [feature]: [Department, file 엔티티 제작 완료] ([2])Department, file 엔티티 제작 완료 파일 저장시 시간에 대해 애기 나눌게 남음

---
+++ src/main/java/com/team09/sb01hrbank09/entity/Department.java
@@ -0,0 +1,66 @@
+package com.team09.sb01hrbank09.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "departments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, nullable = false, length = 20)
+	private String name;
+	@Column(nullable = false, length = 20)
+	private String description;
+	@Column(name = "created_at", nullable = false)
+	private LocalDate createdAt;
+	@Column(name = "employee_count", nullable = false)
+	private int employeeCount = 0;
+
+	private Department( String name, String description) {
+		this.name = name;
+		this.description = description;
+		this.createdAt = LocalDate.now();
+	}
+
+
+
+	public static Department createDepartments(String name, String description) {
+		return new Department(name, description);
+	}
+
+	public void updateDepartmentsName(String updateName) {
+		this.name = updateName;
+	}
+
+	public void updateDepartmentDescription(String updateDescription) {
+		this.description = updateDescription;
+	}
+
+	public void updateCreatedLocalDate() {
+		this.createdAt = LocalDate.now();
+	}
+
+	public void increaseCount() {
+		this.employeeCount++;
+	}
+
+	public void decreaseCount() {
+		this.employeeCount--;
+	}
+
+
+}
