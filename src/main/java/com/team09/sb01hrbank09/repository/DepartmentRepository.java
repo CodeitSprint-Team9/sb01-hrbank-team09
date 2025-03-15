223ab157dcb8928f5bfce520173b7fde43976e78
김승찬 <tmdcks801>
2025-03-15 10:36:00 +0900

    [feature]: [각 레포지토리에 간단한 crud 설정 완료] ([7]) 레포지토리 제작 완료

---
+++ src/main/java/com/team09/sb01hrbank09/repository/DepartmentRepository.java
@@ -0,0 +1,21 @@
+package com.team09.sb01hrbank09.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.team09.sb01hrbank09.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+	Department save(Department department);
+
+	Optional<Department> findById(Long id);
+
+	List<Department> findAll();
+
+	void deleteById(Long id);
+}
