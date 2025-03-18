9709f834a9ef2cd580752b924fa38223f7ba2a90
ghtkdrla <ghtkdrla321@naver.com>
2025-03-18 16:28:54 +0900

    [feature] : [Department Service 구현완료 Testing 필요] #18

--- src/main/java/com/team09/sb01hrbank09/repository/DepartmentRepository.java
+++ src/main/java/com/team09/sb01hrbank09/repository/DepartmentRepository.java
@@ -1,5 +1,6 @@
 package com.team09.sb01hrbank09.repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +15,36 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
 	Department save(Department department);
 
 	Optional<Department> findById(Long id);
+	Optional<Department> findByName(String name);
+
 
 	List<Department> findAll();
 
 	void deleteById(Long id);
+
+	List<Department> findByIdGreaterThanAndNameContainingOrDescriptionContaining(
+			Long idAfter, String nameSearchTerm, String descriptionSearchTerm, Pageable pageable
+	);
+
+	List<Department> findByNameContainingOrDescriptionContaining(
+			String nameSearchTerm, String descriptionSearchTerm, Pageable pageable
+	);
+
+	Long countByIdGreaterThanAndNameContainingOrDescriptionContaining(
+			Long idAfter, String nameSearchTerm, String descriptionSearchTerm
+	);
+
+	Long countByNameContainingOrDescriptionContaining(
+			String nameSearchTerm, String descriptionSearchTerm
+	);
+
+	Optional<Department> findFirstByIdGreaterThanAndNameContainingOrDescriptionContaining(
+			Long idAfter, String nameSearchTerm, String descriptionSearchTerm, Pageable pageable
+	);
+
+	Optional<Department> findFirstByNameContainingOrDescriptionContaining(
+			String nameSearchTerm, String descriptionSearchTerm, Pageable pageable
+	);
+
+
 }
